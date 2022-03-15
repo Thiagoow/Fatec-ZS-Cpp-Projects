39bafb7385a5b00662ba99bd3229f20472dabdb0
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2022-03-15 17:49:36 -0300

    Unicode & accentuation tutorial on Windows moved to helloWorld

--- Atividade1/LO1A.c++
+++ Atividade1/LO1A.c++
@@ -1,23 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 int main(void) {
-  //Deixa acentuação no C++:
-  setlocale(LC_ALL, "Portuguese");
-
-  /*☝🏼 Se nn funcionar: 
-  1- Windows + R -> Digite "intl.cpl";
-  2- Vá na aba "Administrativo" e clique no botão Alterar localidade do sistema;
-  3- Ativar Beta: Use Unicode UTF-8 para suporte a idiomas em todo o mundo;
-  4- Reinicie o sistema.
-  */
-  
   float cel, fah;  
+
   printf("Escreva a temperatura em graus Celsius: ");
   scanf("%f",&cel);
 
   fah = (cel * 9) + 160;
 
-  printf("\nSão %.0f graus Fahrenheit\n", fah);
+  printf("São %.0f graus Fahrenheit\n", fah);
 }
--- HelloWorld.c
+++ HelloWorld.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+//Biblioteca de idiomas do C/Cpp:
+#include <locale.h>
 
 int main() {
-   //Pra executar é só apertar a tecla f6 :D
+ //Deixa acentuação no C++:
+  setlocale(LC_ALL, "Portuguese");
+
+  /*☝🏼 Se nn funcionar: 
+  1- Windows + R -> Digite "intl.cpl";
+  2- Vá na aba "Administrativo" e clique no botão Alterar localidade do sistema;
+  3- Ativar Beta: Use Unicode UTF-8 para suporte a idiomas em todo o mundo;
+  4- Reinicie o sistema.
+  */
+
    char name[] = "Thiago";
    
    printf("Olá mundo, meu nome é %s!\n", name);
 }
+
+/* Pra executar esse código com tdm-gcc + a extensão 
+c/c++ compiler extension, é só apertar a tecla f6 :D */
