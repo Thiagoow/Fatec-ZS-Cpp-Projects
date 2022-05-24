829b322f62542fe59bf40565b30ee896e5d22bd1
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-05-24 11:06:16 -0300

    Exercise done

--- Ativ11/MaiorMenorSalários.cpp
+++ Ativ11/MaiorMenorSalários.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 int main() {
-   const int arraySize = 2;
-   double incomes[arraySize], ages[arraySize], minIncome, maxIncome;
+   const int arraySize = 3;
+   double incomes[arraySize], minIncome, maxIncome;
+   int ages[arraySize], minAge, maxAge;
    string repeat;
 
 Repetir:
@@ -20,18 +21,23 @@ Repetir:
 
    minIncome = incomes[0];
    maxIncome = incomes[0];
+   maxAge = ages[0];
+   minAge = ages[0];
 
    for (int i = 1; i < arraySize; i++) {
       if (minIncome > incomes[i]) {
          minIncome = incomes[i];
+         minAge = ages[i];
       } else if (maxIncome < incomes[i]) {
          maxIncome = incomes[i];
+         maxAge = ages[i];
       }
    }
 
-   cout << "\nO maior salário digitado é: " << maxIncome
-        << ", recebido por: " << endl;
-   cout << "E o menor é: " << minIncome << endl << endl;
+   cout << "\nO maior salário digitado é R$" << maxIncome
+        << ", recebido por uma pessoa de " << maxAge << " anos." << endl;
+   cout << "E o menor é R$" << minIncome << ", recebido por uma pessoa de "
+        << minAge << " anos.\n\n";
 
    cout << "Para não repetir o algoritmo, digite 'N', ou 'n': ";
    cin >> repeat;
