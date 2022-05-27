49c389e6b64af64eceedf0fb367d8de58d386299
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-05-27 00:52:07 -0300

    Formatted code

--- Ativ05/MaiorDos3.cpp
+++ Ativ05/MaiorDos3.cpp
@@ -2,39 +2,40 @@
 using namespace std;
 
 int main() {
-const int arraySize = 3;
-double abc[arraySize], minimum, maximum;
-string repeat;
+   const int arraySize = 3;
+   double abc[arraySize], minimum, maximum;
+   string repeat;
 
-  Repetir:
-    system("cls");
-    cout << "--Digite " << arraySize << " valores--\n"; 
-    
-    for (int i = 0; i < arraySize; i++) {
-      cout << "Digite o valor: "; cin >> abc[i]; 
-    }
-    
-    minimum = abc[0];
-    maximum = abc[0];
+Repetir:
+   system("cls");
+   cout << "--Digite " << arraySize << " número(s)--\n";
 
-    for (int i = 1; i < arraySize; i++) {
+   for (int i = 0; i < arraySize; i++) {
+      cout << "Digite o num: ";
+      cin >> abc[i];
+   }
+
+   minimum = abc[0];
+   maximum = abc[0];
+
+   for (int i = 1; i < arraySize; i++) {
       if (minimum > abc[i]) {
-        minimum = abc[i];
+         minimum = abc[i];
+      } else if (maximum < abc[i]) {
+         maximum = abc[i];
       }
-      else if (maximum < abc[i]) {
-        maximum = abc[i];
-      }
-	  }
-    
-    cout << "\nO maior número é: " << maximum << endl;
-    cout << "O menor número é: " << minimum << endl << endl;
-    
-    cout << "Para não repetir o algoritmo, digite 'N', ou 'n': "; cin >> repeat;
-    if (repeat == "N" || repeat == "n") {
+   }
+
+   cout << "\nO maior número é: " << maximum << endl;
+   cout << "O menor número é: " << minimum << endl << endl;
+
+   cout << "Para não repetir o algoritmo, digite 'N', ou 'n': ";
+   cin >> repeat;
+   if (repeat == "N" || repeat == "n") {
       return 0;
-    }
+   }
 
-  goto Repetir;
+   goto Repetir;
 
-  return 0;
+   return 0;
 }
