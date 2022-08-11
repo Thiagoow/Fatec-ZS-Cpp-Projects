1468482ec88a877a646367a55154d961cb8345f0
Thiagoow <thiagoslopes8@outlook.com>
2022-08-11 11:34:15 -0300

    Test2

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Biblioteca de idiomas do C/Cpp:
+// Biblioteca de idiomas do C/Cpp:
 #include <locale.h>
 
 int main() {
- //Deixa acentuação no C++:
- //setlocale(LC_ALL, "Portuguese");
+   // Deixa acentuação no C++:
+   // setlocale(LC_ALL, "Portuguese");
 
-  /*☝🏼 Se nn funcionar: 
-  1- Windows + R -> Digite "intl.cpl";
-  2- Vá na aba "Administrativo" e clique no botão Alterar localidade do sistema;
-  3- Ativar Beta: Use Unicode UTF-8 para suporte a idiomas em todo o mundo;
-  4- Reinicie o sistema.
-  Se seguiu os passos acima, nn use a biblioteca, pois a msm se encontra DEPRECATED❗
-  */
+   /*☝🏼 Se nn funcionar:
+   1- Windows + R -> Digite "intl.cpl";
+   2- Vá na aba "Administrativo" e clique no botão Alterar localidade do
+   sistema; 3- Ativar Beta: Use Unicode UTF-8 para suporte a idiomas em todo o
+   mundo; 4- Reinicie o sistema. Se seguiu os passos acima, nn use a biblioteca,
+   pois a msm se encontra DEPRECATED❗
+   */
 
-   char name[] = "Thiago";
-   
+   char name[] = "Yo";
+
    printf("Olá mundo, meu nome é %s! :D\n", name);
 }
 
-/* Pra executar esse código com tdm-gcc + a extensão 
+/* Pra executar esse código com tdm-gcc + a extensão
 c/c++ compiler extension, é só apertar a tecla f6 :D */
